c81f7402c5078b9b25d0d9591a53f65e55b42930
lroman272 <luca.a.roman.17@gmail.com>
2024-12-13 17:35:36 -0500

    Better perfection stuff. Scoreable code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/IntakeTest.java
@@ -10,6 +10,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -109,245 +110,58 @@ public class IntakeTest extends LinearOpMode {
 
         if (isStopRequested()) return;
 
-        // Define and start threads for controlling drive and preset
-        Thread driveThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (opModeIsActive()) {
-                    // Handle drive train control
-                    double y = -gamepad1.left_stick_y;
-                    double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-                    double rx = gamepad1.right_stick_x;
+        while (opModeIsActive()) {
+            // Handle drive train control
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
 
-                    double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-                    double frontLeftPower = (y + x + rx) / denominator;
-                    double backLeftPower = (y - x + rx) / denominator;
-                    double frontRightPower = (y - x - rx) / denominator;
-                    double backRightPower = (y + x - rx) / denominator;
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
 
-                    // Drive motor control with speed adjustment via bumpers
-                    if (gamepad1.left_bumper) {
-                        fl.setPower(frontLeftPower / 3);
-                        fr.setPower(frontRightPower / 3);
-                        bl.setPower(backLeftPower / 3);
-                        br.setPower(backRightPower / 3);
-                    } else {
-                        fl.setPower(frontLeftPower);
-                        fr.setPower(frontRightPower);
-                        bl.setPower(backLeftPower);
-                        br.setPower(backRightPower);
-                    }
-
-
-                    // Call viper slide control function
-                    viperSlideControl();
-
-                    // Call angle motor control function
-                    controlAngleMotor();
-                }
+            // Drive motor control with speed adjustment via bumpers
+            if (gamepad1.left_bumper) {
+                fl.setPower(frontLeftPower / 3);
+                fr.setPower(frontRightPower / 3);
+                bl.setPower(backLeftPower / 3);
+                br.setPower(backRightPower / 3);
+            } else {
+                fl.setPower(frontLeftPower);
+                fr.setPower(frontRightPower);
+                bl.setPower(backLeftPower);
+                br.setPower(backRightPower);
             }
-        });
-
-        Thread presetThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (opModeIsActive()) {
-                    // Adjust PID constants in real time using the dashboard sliders
-                    pidController.setP(p);
-                    pidController.setI(i);
-                    pidController.setD(d);
-                    pidController.setF(f);
-
-                    // PID control for holding the arm in position
-                    double currentPosition = extendMotor.getCurrentPosition();
-                    double targetPosition = gamepad2.left_stick_y * MAX_EXTEND_PICKING_UP; // Adjust target based on joystick input
-                    double power = pidController.calculate (currentPosition, targetPosition);
-                    extendMotor.setPower(power); // Apply calculated power to hold position
-
-                    // Display telemetry
-                    telemetry.addData("PID P", p);
-                    telemetry.addData("PID I", i);
-                    telemetry.addData("PID D", d);
-                    telemetry.addData("PID F", f);
-                    telemetry.addData("Extend Motor Position", currentPosition);
-                    telemetry.addData("Target Position", targetPosition);
-                    telemetry.addData("PID Output", power);
-                    telemetry.update();
-
-                    // Preset control
-                    if (gamepad2.a) { // Use Case: Score in top bucket use case
-                        scoreInTopBucket();
-                    }
-                    if (gamepad2.b) { // Use Case: Zero position
-                        zeroPosition();
-                    }
-                    if (gamepad2.x) { // Use Case: Pick up from ground
-                        pickUpFromGround();
-                    }
-                    if (gamepad2.dpad_left) { // Use Case: Pick up specimen
-                        pickUpSpecimen();
-                    }
-                    if (gamepad2.dpad_down) { // Use Case: Clip specimen on top bar
-                        clipSpecimenWait();
-                    }
-                    if (gamepad2.dpad_up) { // Use Case: Clip specimen on high bar
-                        clipSpecimenHighBar();
-                    }
-                    // Call claw control function
-                    controlIntake();
-                }
+            // Display telemetry
+            telemetry.addData("PID P", p);
+            telemetry.addData("PID I", i);
+            telemetry.addData("PID D", d);
+            telemetry.addData("PID F", f);
+            if (gamepad2.right_trigger > 0) {
+                IntakeLeft.setPower(1);
+                IntakeRight.setPower(-1);
+            } else if (gamepad2.left_trigger > 0) {
+                IntakeLeft.setPower(-1);
+            } else {
+                IntakeLeft.setPower(0);
+                IntakeRight.setPower(0);
             }
-        });
 
-        // Start both threads
-        driveThread.start();
-        presetThread.start();
 
-        // Wait for threads to complete
-        try {
-            driveThread.join();
-            presetThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Function to open and close the claw
-    private void controlIntake() {
-        if (gamepad2.right_trigger > 0) {
-            IntakeLeft.setPosition(1);
-            IntakeRight.setPosition(-1);
-        } else if (gamepad2.left_trigger > 0) {
-            IntakeLeft.setPosition(-1);
-        } else {
-            IntakeLeft.setPosition(0);
-            IntakeRight.setPosition(0);
-        }
-    }
-
-        // Function to control the viper slide
-    private void viperSlideControl() {
-        if (gamepad1.right_trigger > 0.5) {
-            if (extendMotor.getCurrentPosition() > ZERO_EXTEND) {
-                extendArmToPosition(extendMotor.getCurrentPosition() - 5);
-                waitForArmExtendPosition();
-            }
-        } else if (gamepad1.left_trigger > 0.5) {
-            double currentAngle = angMotor.getCurrentPosition();
-            double maxExtendLimit = (currentAngle < ANGLE_SCORE_TOP_BUKET - 1000) ? MAX_EXTEND_SCORE_IN_BUCKET : MAX_EXTEND_PICKING_UP;
-            if (extendMotor.getCurrentPosition() < maxExtendLimit) {
-                extendArmToPosition(extendMotor.getCurrentPosition() + 5);
-                waitForArmExtendPosition();
+            if (gamepad1.dpad_up) {
+                angMotor.setPower(1);
+            } else if (gamepad1.dpad_down) {
+                angMotor.setPower(-1);
+            } else if (gamepad1.dpad_right) {
+                extendMotor.setPower(1);
+            } else if (gamepad1.dpad_left) {
+                extendMotor.setPower(-1);
+            } else {
+                extendMotor.setPower(0);
+                angMotor.setPower(0);
             }
         }
-    }
-
-    // Function to control the angle motor
-    private void controlAngleMotor() {
-        if (gamepad2.left_stick_y < 0) {
-            int targetPosition = angMotor.getCurrentPosition() - 5;
-            if (targetPosition > MIN_ARM_ANGLE) {
-                moveArmToPosition(targetPosition);
-                waitForArmAngle();
-            }
-        } else if (gamepad2.left_stick_y > 0) {
-            int targetPosition = angMotor.getCurrentPosition() + 5;
-            if (targetPosition < MAX_ARM_ANGLE) {
-                moveArmToPosition(targetPosition);
-                waitForArmAngle();
-            }
-        }
-    }
-
-    // Function to score in the top bucket
-    private void scoreInTopBucket() {
-        sleep(100);
-        ClawTurn.setPosition(CLAW_SCORE_TOP_BUCKET);
-        extendArmToPosition(EXTEND_HALF);
-        moveArmToPosition(ANGLE_SCORE_TOP_BUKET);
-        waitForArmExtendAndAnglePosition();
-        extendArmToPosition(MAX_EXTEND_SCORE_IN_BUCKET);
-        waitForArmExtendPosition();
-    }
-
-    // Function to move the arm to zero position
-    private void zeroPosition() {
-        extendArmToPosition(ZERO_EXTEND);
-        moveArmToPosition(ANGLE_ZERO);
-        waitForArmExtendAndAnglePosition();
-    }
-
-    // Function to pick up from the ground
-    private void pickUpFromGround() {
-        ClawTurn.setPosition(CLAW_DOWN_FLOOR_EXTEND);
-        extendArmToPosition(ZERO_EXTEND); //test
-        moveArmToPosition(ANGLE_FLOOR_PICK_UP);
-        waitForArmExtendAndAnglePosition();
-        extendArmToPosition(MAX_EXTEND_PICKING_UP);
-        waitForArmExtendPosition();
-    }
-
-    // Function to pick up a specimen
-    private void pickUpSpecimen() {
-        ClawTurn.setPosition(CLAW_SPECIMEN_PICK_UP);
-        extendArmToPosition(ZERO_EXTEND);
-        moveArmToPosition(ANGLE_SPECIMEN_FLOOR_PICK_UP);
-        waitForArmExtendAndAnglePosition();
-    }
-
-    // Function to clip the specimen on the top bar
-    private void clipSpecimenWait() {
-        ClawTurn.setPosition(CLAW_CLIPPING_POSITION);
-        extendArmToPosition(ZERO_EXTEND);
-        moveArmToPosition(ANGLE_ARM_CLIP);
-        waitForArmExtendAndAnglePosition();
-    }
-
-    // Function to clip the specimen on the high bar
-    private void clipSpecimenHighBar() {
-        ClawTurn.setPosition(CLAW_CLIPPING_POSITION);
-        moveArmToPosition(ANGLE_ARM_CLIP);
-        extendArmToPosition(EXTEND_POST_CLIPPING);
-        waitForArmExtendAndAnglePosition();
-        sleep(100);
-    }
-
-    // Helper function to extend the arm to a target position
-    private void extendArmToPosition(double position) {
-        extendMotor.setTargetPosition((int) position);
-        extendMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        extendMotor.setPower(1);
-    }
-
-    // Helper function to move the arm to a target angle
-    private void moveArmToPosition(double angle) {
-        angMotor.setTargetPosition((int) angle);
-        angMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        angMotor.setPower(1);
-    }
-
-    // Helper function to wait for the arm to extend to the target position
-    private void waitForArmExtendAndAnglePosition() {
-        while (extendMotor.isBusy()||angMotor.isBusy()) {
-            // Wait for the arm to reach target
-        }
-        extendMotor.setPower(0);
-        angMotor.setPower(0);
-    }
-
-    // Helper function to wait for the arm to extend to the target position
-    private void waitForArmExtendPosition() {
-        while (extendMotor.isBusy()) {
-            // Wait for the arm to reach target
-        }
-        extendMotor.setPower(0);
-    }
-
-    // Helper function to wait for the arm to move to the target angle
-    private void waitForArmAngle() {
-        while (angMotor.isBusy()) {
-            // Wait for the arm to reach target
-        }
-        angMotor.setPower(0);
     }
 }
