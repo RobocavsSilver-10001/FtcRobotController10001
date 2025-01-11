adcd788c4787d63e516f409f2573bc33f39e0bef
Robocavs <robocavssilver10001@gmail.com>
2025-01-11 01:41:29 -0500

    1/10/2025 6:33PM. Claw grab positions fixed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousClaw/Finalized/BucketAutos/AutoTest1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousClaw/Finalized/BucketAutos/AutoTest1.java
@@ -1,34 +1,34 @@
-
-package org.firstinspires.ftc.teamcode.AutonomousClaw.Finalized.BucketAutos;
+/*package org.firstinspires.ftc.teamcode.AutonomousClaw.Finalized.BucketAutos;
 
 import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.teamcode.DefaultRRFiles.MecanumDrive;
-
 @Autonomous(name = "DUB")
 public class AdvancedRedBucket extends LinearOpMode {
-    //
+
+    public DcMotorEx angMotor, extendMotor;
+    public Servo ClawGrab, ClawTurn;
 
     @Override
     public void runOpMode() {
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(-8, -60, Math.toRadians(90)));
-        Servo ClawGrab = hardwareMap.servo.get("ClawGrab");
-        Servo ClawTurn = hardwareMap.servo.get("ClawTurn");
-        DcMotor angMotor = hardwareMap.dcMotor.get("AngleMotor");
-        DcMotor extendMotor = hardwareMap.dcMotor.get("ExtendMotor");
+
+        ClawGrab = hardwareMap.servo.get("ClawGrab");
+        ClawTurn = hardwareMap.servo.get("ClawTurn");
+        angMotor = hardwareMap.dcMotor.get("AngleMotor");
+        extendMotor = hardwareMap.dcMotor.get("ExtendMotor");
 
         angMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         angMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -40,41 +40,45 @@ public class AdvancedRedBucket extends LinearOpMode {
 
         waitForStart();
 
+        // Run the actions in parallel and sequential order
         Actions.runBlocking(new SequentialAction(
                 drive.actionBuilder(new Pose2d(-8, -60, Math.toRadians(90)))
-                        .stopAndAdd(new ClawRelease(ClawGrab, .389))
-                        .stopAndAdd(new ClawAngle(ClawTurn, 0.33))
-                        .stopAndAdd(new ArmAngle(angMotor, 3200))
-                        .splineToConstantHeading(new Vector2d(-5 , -36.5), Math.toRadians(90))
-                        .stopAndAdd(new ArmExtension(extendMotor, 650))
+                        .stopAndAdd(new ParallelAction(
+                                new ClawRelease(ClawGrab, .389),
+                                new ClawAngle(ClawTurn, 0.33),
+                                new ArmAngle(angMotor, 2890)
+                        ))
+                        .splineToConstantHeading(new Vector2d(-10 , -35.95), Math.toRadians(90))
+                        .stopAndAdd(new ArmExtension(extendMotor, 860))
                         .waitSeconds(1)
                         .stopAndAdd(new ClawRelease(ClawGrab, .465))
                         .waitSeconds(1)
                         .lineToYConstantHeading(-50)
-                        .stopAndAdd(new ArmExtension(extendMotor, 0))
-                        .stopAndAdd(new ArmAngle(angMotor, -2640))
-                        .stopAndAdd(new ClawAngle(ClawTurn, .33))
-                        .splineToConstantHeading(new Vector2d(-44.5 , -40), Math.toRadians(90))
+
+                        // Second ParallelAction
+                        .stopAndAdd(new ParallelAction(
+                                new ArmExtension(extendMotor, 0),
+                                new ArmAngle(angMotor, -2615),
+                                new ClawAngle(ClawTurn, .33)
+                        ))
+                        .splineToConstantHeading(new Vector2d(-46.25 , -39), Math.toRadians(90))
                         .waitSeconds(.5)
-                        .stopAndAdd(new ArmExtension(extendMotor, 268))
+                        .stopAndAdd(new ArmExtension(extendMotor, 295))
                         .stopAndAdd(new ClawRelease(ClawGrab, .389))
-                        .waitSeconds(.5)
+                        .stopAndAdd(new ArmAngle(angMotor, 600))
+                        .turn(2.45)
                         .stopAndAdd(new ArmAngle(angMotor, 3800))
-                        .waitSeconds(2)
-                        .stopAndAdd(new ArmExtension(extendMotor, 2900))
-                        .splineToConstantHeading(new Vector2d(-49.5 , -40), Math.toRadians(215))
-                        /*.lineToX(-49)
+                        .waitSeconds(.1)
+                        .stopAndAdd(new ArmExtension(extendMotor, 2100))
+                        .waitSeconds(.1)
+                        .lineToX(-49)
                         .stopAndAdd(new ClawRelease(ClawGrab, .465))
                         .strafeToLinearHeading(new Vector2d(-50, -46), Math.toRadians(90))
                         .stopAndAdd(new ArmExtension(extendMotor, 0))
-                        .stopAndAdd(new ArmAngle(angMotor, 0))*/
-                        .build()));
-
-
-        // OPENS THE CLAW .stopAndAdd(new ClawRelease(ClawGrab, 0.55)) */
-        //arm angle
+                        .stopAndAdd(new ArmAngle(angMotor, 0))
+                        .build()
+        ));
     }
-
 
     public class ClawRelease implements Action {
         Servo ClawGrab;
@@ -100,12 +104,12 @@ public class AdvancedRedBucket extends LinearOpMode {
             this.ClawTurn = CT;
             this.TurnPosition = TP;
         }
+
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
             ClawTurn.setPosition(TurnPosition);
             return false;
         }
-
     }
 
     public class ArmAngle implements Action {
@@ -121,7 +125,6 @@ public class AdvancedRedBucket extends LinearOpMode {
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
             angMotor.setTargetPosition(anglePosition);
             angMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
             angMotor.setPower(1);
             return angMotor.isBusy();
         }
@@ -140,9 +143,9 @@ public class AdvancedRedBucket extends LinearOpMode {
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
             extendMotor.setTargetPosition(extendPosition);
             extendMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            extendMotor.setPower(.5);
+            extendMotor.setPower(1);
             return extendMotor.isBusy();
         }
     }
 }
+*/
